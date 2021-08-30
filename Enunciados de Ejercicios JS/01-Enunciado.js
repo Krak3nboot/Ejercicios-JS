f8184582f80def9ce3c9950ed4dd4d94684eef0f
Krak3nboot <54318121+Krak3nboot@users.noreply.github.com>
2021-08-30 15:45:39 -0300

    Ejercicios JS

---
+++ Enunciados de Ejercicios JS/01-Enunciado.js
@@ -0,0 +1,93 @@
+/*
+                        Ejercicio 3.9.1.
+
+Escribir dos funciones que permitan calcular:
+a) La duración en segundos de un intervalo dado en horas, minutos y segundos.
+b) La duración en horas, minutos y segundos de un intervalo dado en segundos.
+
+
+                        Ejercicio 3.9.2. 
+
+Usando las funciones del ejercicio anterior, escribir un programa que pida al
+usuario dos intervalos expresados en horas, minutos y segundos, sume sus duraciones, y muestre
+por pantalla la duración total en horas, minutos y segundos.
+
+
+                        Ejercicio 3.9.3. 
+
+Escribir una función que, dados cuatro números, devuelva el mayor producto
+de dos de ellos. Por ejemplo, si recibe los números 1, 5, -2, -4 debe devolver 8, que es el producto
+más grande que se puede obtener entre ellos (8 = −2 × −4). 
+
+
+                        Ejercicio 4.6.4. 
+
+Escribir funciones que permitan encontrar:
+a) El máximo o mínimo de un polinomio de segundo grado (dados los coeficientes a, b y
+c), indicando si es un máximo o un mínimo.
+b) Las raíces (reales o complejas) de un polinomio de segundo grado.
+Nota: validar que las operaciones puedan efectuarse antes de realizarlas (no dividir por
+cero, ni calcular la raíz de un número negativo).
+c) La intersección de dos rectas (dadas las pendientes y ordenada al origen de cada recta).
+Nota: validar que no sean dos rectas con la misma pendiente, antes de efectuar la operación.
+
+
+                        Ejercicio 4.6.5. 
+
+Escribir funciones que resuelvan los siguientes problemas:
+a) Dado un año indicar si es bisiesto.
+Nota: un año es bisiesto si es un número divisible por 4, pero no si es divisible por 100,
+excepto que también sea divisible por 400.
+b) Dado un mes y un año, devolver la cantidad de días correspondientes.
+c) Dada una fecha (día, mes, año), indicar si es válida o no.
+d) Dada una fecha, indicar los días que faltan hasta fin de mes.
+e) Dada una fecha, indicar los días que faltan hasta fin de año.
+f) Dada una fecha, indicar la cantidad de días transcurridos en ese año hasta esa fecha.
+g) Dadas dos fechas (día1, mes1, año1, día2, mes2, año2), indicar el tiempo transcurrido
+entre ambas, en años, meses y días.
+Nota: en todos los casos, invocar las funciones escritas previamente cuando sea posible.
+
+                        Ejercicio 4.6.8.
+
+Programa de astrología: el usuario debe ingresar el día y mes de su cumpleaños
+y el programa le debe decir a qué signo corresponde.
+
+Aries: 21 de marzo al 20 de abril. Tauro: 21 de abril al 20 de mayo.
+Geminis: 21 de mayo al 21 de junio. Cancer: 22 de junio al 23 de julio.
+Leo: 24 de julio al 23 de agosto. Virgo: 24 de agosto al 23 de septiembre.
+Libra: 24 de septiembre al 22 de octubre. Escorpio: 23 de octubre al 22 de noviembre.
+Sagitario: 23 de noviembre al 21 de diciembre. Capricornio: 22 de diciembre al 20 de enero.
+Acuario: 21 de enero al 19 de febrero. Piscis: 20 de febrero al 20 de marzo
+
+
+                        Ejercicio 5.7.3. 
+Manejo de contraseñas
+
+a) Escribir un programa que contenga una contraseña inventada, que le pregunte al usuario
+la contraseña, y no le permita continuar hasta que la haya ingresado correctamente.
+b) Modificar el programa anterior para que solamente permita una cantidad fija de intentos.
+d) Modificar el programa anterior para que sea una función que devuelva si el usuario
+ingresó o no la contraseña correctamente, mediante un valor booleano (True o False).
+
+                        Ejercicio 5.7.6. 
+Potencias de dos
+
+a) Escribir una función es_potencia_de_dos que reciba como parámetro un número natural,
+y devuelva True si el número es una potencia de 2, y False en caso contrario.
+b) Escribir una función que, dados dos números naturales pasados como parámetros,
+devuelva la suma de todas las potencias de 2 que hay en el rango formado por
+esos números (0 si no hay ninguna potencia de 2 entre los dos). Utilizar la función
+es_potencia_de_dos, descripta en el punto anterior.
+
+
+                        Ejercicio 5.7.8.  
+
+Escribir un programa que le pida al usuario que ingrese una sucesión de números
+naturales (primero uno, luego otro, y así hasta que el usuario ingrese ’-1’ como condición
+de salida). Al final, el programa debe imprimir cuántos números fueron ingresados, la suma
+total de los valores y el promedio.
+
+
+
+
+*/
